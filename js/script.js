fa84e01cb7cb48f9ead124ac959a75adbd49a415
ByrdBassPC <byrdbass@gmail.com>
2022-01-07 10:01:44 -0600

    made a some style on description class marks that may need to be adjusted with existing classes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var currentDateTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 console.log(currentDateTime);
 //create a variable for the HTML element that will display the current Date and Time
     //COULD WE CHANGE THE STYLING ON THIS TO GIVE MORE OF A SPACE BEFORE THE FOLLOWING CODE???
-var displayCurrentDateTime = $('#currentDay')
+var displayCurrentDateTime = $('#Today')
         // WILL THE CALENDAR ALWAYS DISPLAY THE CORRECT TIME ZONE FOR THE USER???
 // create a display of the current day using moment.js
 var currentDay = moment().format('dddd, MMMM Do')
@@ -13,7 +13,7 @@ displayCurrentDateTime.text(currentDay);
 var workDay = 8
 //find a way to display all of the hourly time blocks for the work day
 var typeDescription = $('.description')
-//typeDescription.text("blah")
+typeDescription.text("blah")
 //we may have to use 3 classes (.time-block, .row, and .hour) for each hour frm 9am to 5pm
     //we will need to add the 4th class (of either .past, .present, or .future) to these existing 3
 // create a arguement within moment.js that calculates the present hour + or - the time of day between 9am and 5pm
