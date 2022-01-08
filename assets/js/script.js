436b0203729596421a1b3140e95d1973aff94c05
ByrdBassPC <byrdbass@gmail.com>
2022-01-07 19:20:49 -0600

    found a way to define other classes for past and future & attempted dynamic time variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 //link the current day in with html class "currentDay"
-var exactCurrentDateTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-console.log(exactCurrentDateTime);
+var exactCurrentDateTime = moment().format('dddd, MMMM Do YYYY, hh:mm:ss a')
+console.log("The exact current time is " + exactCurrentDateTime)
+var displayCurrentDateTime = $('#Today')
+displayCurrentDateTime.text(exactCurrentDateTime);
+//add the function from moment for current hour
+var currentHour = moment().hour();
+console.log("The current hour in military time is " + currentHour);
+var currentTimeSlot = currentHour;
+console.log("the current time slot is " + currentTimeSlot);
+var minimumTime = 9
+var maximumTime = 17
+var previousTime = moment().diff(minimumTime, 'hours').format('hh');
+console.log("the Previous time is " + previousTime + " hours");
+var futureTime = moment().add(maximumTime, 'hours').format('hh')
+console.log("the future time is " + previousTime + " hours");
 //experimenting with id class matching current time
-var pastTimeSlot = $('9am')
-pastTimeSlot.className
+//Need to get id's to match up with current hour
+var pastTimeSlot = $("#9am");
+console.log("the past time slots are " + pastTimeSlot);
+pastTimeSlot.addClass("past");
 var presentTimeSlot = $('#10am'); //will need to replace the current element with moment.js function
-console.log(presentTimeSlot);
-presentTimeSlot.className = ".present";
-console.log(presentTimeSlot.className)
+console.log("the current time slot is " + presentTimeSlot);
+presentTimeSlot.addClass("present");
+var futureTimeSlot = $('#11am');
+futureTimeSlot.addClass("future");
+console.log("The Future time slots are " + futureTimeSlot);
 //I am traversing the document to see if i am able to view the CSS in the console for property
-console.log(document.styleSheets[3]);
+//console.log(document.styleSheets[3]);
 //create an array of id's for each time slot
 // presentTimeSlot.cssHooks(".present");
 //if presentTimeSlot is matching exactCurrentDateTime display the CSS property for present
 
 //create a variable for the HTML element that will display the current Date and Time
     //COULD WE CHANGE THE STYLING ON THIS TO GIVE MORE OF A SPACE BEFORE THE FOLLOWING CODE???
-var displayCurrentDateTime = $('#Today')
+
         // WILL THE CALENDAR ALWAYS DISPLAY THE CORRECT TIME ZONE FOR THE USER???
 // create a display of the current day using moment.js
-var currentDay = moment().format('dddd, MMMM Do')
-console.log(currentDay)
-displayCurrentDateTime.text(currentDay);
+
 //create a series of hourly time blocks from 9am to 5pm - 8 hours
 var workDay = 8
 //find a way to display all of the hourly time blocks for the work day
